777768969f124eab3937966c510e5d56f5d044e5
bankbab1 <57855692+bankbab1@users.noreply.github.com>
2026-05-15 21:57:12 +0700

    Chok moded

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -72,14 +72,13 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     meta: [
       { charSet: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { title: "Lovable App" },
-      { name: "description", content: "Lovable Generated Project" },
-      { name: "author", content: "Lovable" },
-      { property: "og:title", content: "Lovable App" },
-      { property: "og:description", content: "Lovable Generated Project" },
+      { title: "Approval Stage Logic" },
+      { name: "description", content: "Generated Project" },
+      { name: "author", content: "Chok" },
+      { property: "og:title", content: "Chok App" },
+      { property: "og:description", content: "Chok Generated Project" },
       { property: "og:type", content: "website" },
-      { name: "twitter:card", content: "summary" },
-      { name: "twitter:site", content: "@Lovable" },
+     
     ],
     links: [
       {
